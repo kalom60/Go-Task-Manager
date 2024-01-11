c925aae155b3a69d3d9a0b5404ed34add5a30f6f
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-01-11 12:11:13 +0300

    db setup

---
+++ server/common/db.go
@@ -0,0 +1,36 @@
+package common
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var db *mongo.Database
+
+func GetDBCollection(col string) *mongo.Collection {
+	return db.Collection(col)
+}
+
+func InitDB() error {
+	uri := os.Getenv("MONGODB_URL")
+	if uri == "" {
+		return errors.New("you must set your 'MONGODB_URI' environmental variable")
+	}
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	if err != nil {
+		return err
+	}
+
+	db = client.Database("Todo")
+
+	return nil
+}
+
+func CloseDB() error {
+	return db.Client().Disconnect(context.Background())
+}
