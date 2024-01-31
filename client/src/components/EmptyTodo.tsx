46b32f40ac8f4efb7b3e17ed735378bdda72765b
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-01-31 20:07:36 +0300

    empty todo component

--- client/src/components/EmptyTodo.tsx
+++ client/src/components/EmptyTodo.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import TodoLogo from "../assets/check copy.png";
+// import List from "../assets/list.png";
+// import TODO from "../assets/to-do-list copy.png";
 
-type Props = {
-  message: string;
-};
-
-const EmptyTodo: React.FC<Props> = ({ message }) => {
+const EmptyTodo: React.FC = () => {
   return (
-    <div className="card">
-      <div className="card-content">
-        <div className="content">{message}</div>
+    <div
+      className="columns is-vcentered is-centered is-mobile is-flex is-flex-direction-column"
+      style={{ height: "600px" }}
+    >
+      <img
+        src={TodoLogo}
+        alt="Todo"
+        style={{ width: "250px", height: "250px" }}
+      />
+      <div className="is-size-4 has-text-weight-bold mt-5 pr-6">
+        No Todo Found
       </div>
     </div>
   );
