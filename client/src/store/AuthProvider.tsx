e02a60d0cd469a177f80e77663469c1a60509c21
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-02-14 19:46:49 +0300

    use api servies for login, logout, and signup

--- client/src/store/AuthProvider.tsx
+++ client/src/store/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useState } from "react";
 import AuthContext from "./auth-context";
+import { loginUser, logoutUser, signUpUser } from "../service/api";
 
 type Props = {
   children: ReactNode;
@@ -9,28 +10,82 @@ type State = {
   isLoggedIn: boolean;
 };
 
+export type LoginInfo = {
+  email: string;
+  password: string;
+};
+
+export type SignUpInfo = {
+  username: string;
+  email: string;
+  password: string;
+};
+
 const AuthProvider: React.FC<Props> = ({ children }) => {
   const [auth, setAuth] = useState<State>({
     isLoggedIn: !!localStorage.getItem("access"),
   });
 
-  const login = () => {
-    setAuth((prevState) => ({
-      ...prevState,
-      isLoggedIn: !!localStorage.getItem("access"),
-    }));
+  const signUp = async (body: LoginInfo) => {
+    try {
+      await signUpUser(JSON.stringify(body));
+      setAuth((prevState) => ({
+        ...prevState,
+        isLoggedIn: !!localStorage.getItem("access"),
+      }));
+    } catch (err) {
+      if (
+        err &&
+        typeof err === "object" &&
+        "status" in err &&
+        "message" in err
+      ) {
+        if (typeof err.message === "string") {
+          throw new Error(err.message);
+        }
+      }
+    }
   };
 
-  const logout = () => {
-    setAuth((prevState) => ({
-      ...prevState,
-      isLoggedIn: !!localStorage.getItem("access"),
-    }));
+  const login = async (body: LoginInfo) => {
+    try {
+      await loginUser(JSON.stringify(body));
+      setAuth((prevState) => ({
+        ...prevState,
+        isLoggedIn: !!localStorage.getItem("access"),
+      }));
+    } catch (err) {
+      if (
+        err &&
+        typeof err === "object" &&
+        "status" in err &&
+        "message" in err
+      ) {
+        if (typeof err.message === "string") {
+          throw new Error(err.message);
+        }
+      }
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await logoutUser();
+      setAuth((prevState) => ({
+        ...prevState,
+        isLoggedIn: !!localStorage.getItem("access"),
+      }));
+    } catch (err) {
+      setAuth((prevState) => ({
+        ...prevState,
+        isLoggedIn: !!localStorage.getItem("access"),
+      }));
+    }
   };
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: auth.isLoggedIn, login, logout }}
+      value={{ isLoggedIn: auth.isLoggedIn, login, logout, signUp }}
     >
       {children}
     </AuthContext.Provider>
