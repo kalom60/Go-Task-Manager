5414cc842139b2249829376ac1d8bb36b6a9193a
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-06-26 23:19:23 +0300

    sign-up

--- client-svelteKit/src/routes/(auth)/signup/+page.svelte
+++ client-svelteKit/src/routes/(auth)/signup/+page.svelte
@@ -1,7 +1,39 @@
-<script>
-	import SignUp from '$lib/components/auth/SignUp.svelte';
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
 </script>
 
-<div class="flex items-center justify-center mt-36">
-	<SignUp />
+<div class="mt-36 flex items-center justify-center">
+	<Card.Root class="w-[450px]">
+		<Card.Header>
+			<Card.Title>Sign Up</Card.Title>
+			<Card.Description
+				>Already have an account?
+				<a href="/signin" class="text-indigo-500">Sign In</a>
+			</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<form>
+				<div class="grid w-full items-center gap-6">
+					<div class="flex flex-col space-y-1.5">
+						<Label for="username">Username</Label>
+						<Input id="name" />
+					</div>
+					<div class="flex flex-col space-y-1.5">
+						<Label for="email">Email</Label>
+						<Input id="email" />
+					</div>
+					<div class="flex flex-col space-y-1.5">
+						<Label for="password">Password</Label>
+						<Input id="password" />
+					</div>
+				</div>
+			</form>
+		</Card.Content>
+		<Card.Footer class="flex justify-between">
+			<Button class="bg-green-500">Sign Up</Button>
+		</Card.Footer>
+	</Card.Root>
 </div>
